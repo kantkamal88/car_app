ce6e7899b13fa7674eef6baf9728ba02926e5a8e
kamal <rickyrohila02@gmail.com>
2025-04-05 11:35:44 +0530

    conflitcts problem solved

--- Demo.java
+++ Demo.java
@@ -1,10 +1,9 @@
 class Demo{
-<<<<<<< HEAD
+
 public static void main (String arg[]){
 	System.out.println("This is Demo classs");
-=======
  public static void main (String arg[]){
 	System.out.printl("In Demo class");
->>>>>>> bbb765a5b9bb79baad02f20fafaffadc83a86a42
+
 	}
 }
