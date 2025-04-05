4a8fcf355ffe103780fe63239cb57b69b582589e
kamal <rickyrohila02@gmail.com>
2025-04-05 11:46:55 +0530

    m2 method completed

--- Demo.java
+++ Demo.java
@@ -1,5 +1,11 @@
 class Demo{
 
+void m2(){
+int a=10, b=20;
+int c=a+b;
+System.out.println("C: "+c);
+}
+
 public static void main (String arg[]){
 	System.out.println("This is Demo classs");
  public static void main (String arg[]){
