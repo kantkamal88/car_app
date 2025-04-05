92c6d2cd5d5fdccfcd8ee149a02e96badb60c81c
kamal <rickyrohila02@gmail.com>
2025-04-05 11:55:47 +0530

    m1 method recoverd by using- $ git stash apply

--- Demo.java
+++ Demo.java
@@ -1,10 +1,23 @@
 class Demo{
 
+<<<<<<< Updated upstream
 void m2(){
 int a=10, b=20;
 int c=a+b;
 System.out.println("C: "+c);
 }
+
+void m1(){
+int i=50, j=30;
+System.out.println("m1() in Demo");
+int k=i+j;
+System.out.println("k: "+k);
+
+
+} 
+
+
+
 
 public static void main (String arg[]){
 	System.out.println("This is Demo classs");
